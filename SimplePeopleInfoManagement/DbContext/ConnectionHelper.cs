33f97236c429ace3c94d7bcce346ba70fd42d1c1
Saeed <Saeed7394@gmail.com>
2021-01-08 00:58:07 +0330

    Add category form and enter new data Add Person form category box and some elements Correct Designer of Form Main in .csproject

---
+++ SimplePeopleInfoManagement/DbContext/ConnectionHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace SimplePeopleInfoManagement.DbContext
+{
+    public static class ConnectionHelper
+    {
+        public static PeopleInfoDbContext getDbConntext()
+        {
+            string con = System.Configuration.ConfigurationManager.ConnectionStrings["PeopleInfoDb"]
+                .ConnectionString;
+            return new PeopleInfoDbContext(con);
+        }
+    }
+}
